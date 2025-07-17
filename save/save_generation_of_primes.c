cd7738b6900401521beb636988fa2b35fdf1467c
wiktorszczepanik <wiktorszczepanik036@gmail.com>
2025-07-17 23:56:17 +0200

    Save to file update

--- save/save_generation_of_primes.c
+++ save/save_generation_of_primes.c
@@ -3,10 +3,10 @@
 #include "save_generation_of_primes.h"
 
 //...
-void save_to_file(bool *is_prime, char file_name[], NumberRange numer_range) {
-    size_t number_of_primes = 0;
-    for (unsigned int i = 0; i < numer_range.b; i++)
-        if (is_prime[i]) number_of_primes++;
+void save_to_file(bool *is_prime, char file_name[], NumberRange number_range) {
+    // size_t number_of_primes = 0;
+    // for (unsigned int i = 0; i < number_range.b; i++)
+    //     if (is_prime[i]) number_of_primes++;
 
     FILE* file = fopen(file_name, "wb");
     if (file == NULL) {
@@ -14,11 +14,16 @@ void save_to_file(bool *is_prime, char file_name[], NumberRange numer_range) {
         exit(EXIT_FAILURE);
     }
 
-    size_t written = fwrite(is_prime, sizeof(int), number_of_primes, file);
-    if (written != number_of_primes) {
-        fclose(file);
-        fprintf(stderr, "Error: Problem with writting to file.\n");
-        exit(EXIT_FAILURE);
+    for (unsigned int i = 0; i < number_range.b; i++) {
+        if (is_prime[i]) {
+            size_t written = fwrite(&i, sizeof(unsigned int), 1, file);
+            if (written != 1) {
+                fclose(file);
+                fprintf(stderr, "Error: Problem with writting to file.\n");
+                exit(EXIT_FAILURE);
+            }
+        }
     }
+
     fclose(file);
 }
